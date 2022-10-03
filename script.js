84f1c5bb85df6e90b1ec513983caf0cf24a093c8
Alistair MacDonald <F1LT3R@users.noreply.github.com>
2022-10-03 10:39:59 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('button#foo')
+    .addEventListener('click', function () {
+        console.log('You clicked me!');                     
+    });
+})
+
+
